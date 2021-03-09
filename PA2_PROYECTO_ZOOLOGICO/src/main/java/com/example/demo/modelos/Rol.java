741c2c8dd4de8ffd0be74b62af7b42b3ea21fd49
Brayan_Alvarez <10brayanalvarez@gmial.com>
2021-03-09 14:43:08 -0600

    Commit

---
+++ PA2_PROYECTO_ZOOLOGICO/src/main/java/com/example/demo/modelos/Rol.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.modelos;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Buddys
+ */
+@Entity
+@Table(name = "tb_roles")
+public class Rol implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String rolNombre;
+    private String rolDescripcion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(String rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+
+    public String getRolDescripcion() {
+        return rolDescripcion;
+    }
+
+    public void setRolDescripcion(String rolDescripcion) {
+        this.rolDescripcion = rolDescripcion;
+    }
+}
