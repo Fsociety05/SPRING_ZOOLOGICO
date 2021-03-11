6487dc5bf2cc1f4b1f22ffb3271ed02a0b8d1c26
Brayan_Alvarez <10brayanalvarez@gmial.com>
2021-03-11 01:38:47 -0600

    Itinerarios, mantenimiento_itinerario, ver_itinerario

---
+++ PA2_PROYECTO_ZOOLOGICO/src/main/java/com/example/demo/controladores/ItinerarioUIControlador.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.controladores;
+
+import com.example.demo.modelos.Habitats;
+import com.example.demo.modelos.Itinerario;
+import com.example.demo.servicios.HabitatsServicios;
+import com.example.demo.servicios.ItinerarioServicios;
+import com.example.demo.servicios.VegetacionServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Buddys
+ */
+@Controller
+public class ItinerarioUIControlador {
+
+    @Autowired
+    private ItinerarioServicios servicio;
+
+    @Autowired
+    private HabitatsServicios servicioHabitat;
+
+
+    @RequestMapping("/mantenimiento_itinerario")
+    public String irMantenimiento(Model model) {
+        
+        Itinerario[] arreglo = new Itinerario[servicio.getTodos().size()];
+        int i = 0;
+        for (Itinerario itinerario : servicio.getTodos()) {
+            arreglo[i] = itinerario;
+            i++;
+        }
+        
+        for (int j = 0; j < arreglo.length; j++) {
+            arreglo[j].setNombreHabitat(servicioHabitat.getUno(arreglo[j].getId_habitat()).getNombre()+"");
+        }
+        
+        setParametro(model, "lista_Itinerario", servicio.getTodos());
+        return "paginas/mantenimiento_itinerario";
+    }
+
+    @RequestMapping("/vista_itinerario")
+    public String vista(Model model) {
+        setParametro(model, "listaItinerario", servicio.getTodos());
+        return "paginas/vista_Itinerario";
+    }
+
+    @GetMapping("/crear_itinerario")
+    public String irCrear_itinerario(Model model) {
+        setParametro(model, "itinerario", new Habitats());
+        setParametro(model, "lista_habitat_combo", servicioHabitat.getTodos());
+        return "paginas/formItinerario";
+    }
+
+    @GetMapping("/actualizarItinerario/{id}")
+    public String irActualizar(@PathVariable("id") Long id, Model modelo) {
+        setParametro(modelo, "itinerario", servicio.getValor(id));
+        setParametro(modelo, "lista_habitat_combo", servicioHabitat.getTodos());
+        return "paginas/formItinerario";
+    }
+
+    @PostMapping("/guardarItinerario")
+    public String guardar(Itinerario itinerario, Model model) {
+        servicio.guardar(itinerario);
+        return "redirect:/mantenimiento_itinerario";
+    }
+
+    @GetMapping("eliminarItinerario/{id}")
+    public String eliminar(@PathVariable("id") Long id, Model modelo) {
+        servicio.eliminar(id);
+        return "redirect:/mantenimiento_itinerario";
+    }
+
+    public void setParametro(Model model, String atributo, Object valor) {
+        model.addAttribute(atributo, valor);
+    }
+
+}
---
+++ PA2_PROYECTO_ZOOLOGICO/src/main/java/com/example/demo/modelos/Itinerario.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.modelos;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Buddys
+ */
+@Data
+@NoArgsConstructor
+@Entity
+public class Itinerario implements Serializable{
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idItinerario;
+    private String nombre;
+    private String horaIncio;
+    private String horaFin;
+    private String duracionEstimada;
+    private String area;
+    private String numeroVisitantes;
+    private Long id_habitat;
+    private String nombreHabitat;
+}
